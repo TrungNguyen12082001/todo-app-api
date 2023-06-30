45199cf8578586f7c759e6d938a5cfd1bfbec1fc
TrungNguyen <nguyentattrung1208@gmail.com>
2023-06-30 09:50:32 +0700

    Adding Primary Key to Task Entity

--- src/tasks/tasks.entity.ts
+++ src/tasks/tasks.entity.ts
@@ -1,6 +1,9 @@
-import { Entity } from 'typeorm';
-import { Priority } from '../enums/Priotity';
-import { Status } from '../enums/Status';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+// import { Priority } from '../enums/Priotity';
+// import { Status } from '../enums/Status';
 
 @Entity()
-export class Task {}
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+}
